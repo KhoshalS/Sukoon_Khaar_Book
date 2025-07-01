54d59f7ad9e95aa7d87d2116730fb6a99b3642c5
Khoshal Saidy <khoshalkdrr@gmail.com>
2025-07-01 23:00:27 +0430

    first_2_1

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
-import 'package:in_app_purchase/in_app_purchase.dart';
+// import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:provider/provider.dart';
 import 'package:sakoon_shar/CustomColors.dart';
 import 'package:sakoon_shar/FavoritePageStorage.dart';
